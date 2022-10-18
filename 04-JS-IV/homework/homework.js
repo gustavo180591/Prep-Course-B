71ff1a1c91983a813641b0e0bc5d6c98d9d964c7
Gustavo <gustavo.faccendini@gmail.com>
2022-10-18 00:04:17 -0300

    first commit

--- 04-JS-IV/homework/homework.js
+++ 04-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var newcat = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      return "Meow!";
+    }
+  } ;
+  return newcat;
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +21,7 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+ 
 }
 
 function invocarMetodo (objeto, metodo) {
